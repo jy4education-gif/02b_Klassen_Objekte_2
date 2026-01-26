d62a1cd829e01dc960e34260a3a0eae2edb8c3bc
jy4education_git <jy4education@gmail.com>
2026-01-26 12:44:01 +0100

    Eigenschaften in einer Klasse angelegt

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,14 @@ package com.cc.java;
 
 public class Cat {
 
+    // Felder / Eigenschaften / Attribute
+    public String firstName;
+    public String furColo;  
+    public int age;
+
+
+
+    
     // return der Instanzvariablen / Adresse
     public Cat getInstanceVariable(){
        return this;
