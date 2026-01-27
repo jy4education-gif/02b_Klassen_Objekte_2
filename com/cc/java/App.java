e0a3e02b434e86f48861f97a3613f83f2004294d
jy4education_git <jy4education@gmail.com>
2026-01-27 09:53:40 +0100

    +

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,15 +5,30 @@ public class App {
     public static void main(String[] args) {
 
         Cat cat = new Cat();
-        output("Blick von aussen: " + cat);  // "Blick von aussen: " ist ein Objekt der Klasse String
+        output("Blick von aussen: " + cat); // implizite Typumwandlung
         output("Blick von innen: " + cat.getInstanceVariable());
 
+        cat.firstName = "Grizabella";
+        cat.furColor = "white";
+        cat.age = 29;
+
+        output(cat.firstName);
+        output(cat.furColor);
+        output(Integer.toString(cat.age)); // explizite Typumwandlung
+        // output(String.valueOf(cat.age));
         output("-----------------------");
 
         Cat cat2 = new Cat();
         output("Blick von aussen: " + cat2);
         output("Blick von innen: " + cat2.getInstanceVariable());
 
+        cat2.firstName = "Alonzo";
+        cat2.furColor = "grey";
+        cat2.age = 35;
+
+        output(cat2.firstName);
+        output(cat2.furColor);
+        output(Integer.toString(cat2.age)); // explizite Typumwandlung
     }
 
   
@@ -23,4 +38,3 @@ public class App {
 
 
 }
-
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -4,12 +4,10 @@ public class Cat {
 
     // Felder / Eigenschaften / Attribute
     public String firstName;
-    public String furColo;  
-    public int age;   // in tist ein primitiv Datentyp
+    public String furColor;
+    public int age;
 
-
-
-
+    
     // return der Instanzvariablen / Adresse
     public Cat getInstanceVariable(){
        return this;
